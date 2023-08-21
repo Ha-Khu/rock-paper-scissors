2831d939903ed73fc8538bca8d16612590cbf645
Dávid <141168015+Ha-Khu@users.noreply.github.com>
2023-08-21 17:15:08 +0200

    Added reset game function

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function getComputerChoice() {
 const player_score_DOM = document.getElementById("player-score");
 const pc_score_DOM = document.getElementById("pc-score");
 const reset_game_DOM = document.getElementById("reset-game");
-const game_status_DOM = document.getElementById("game-status");
 const round_status_DOM = document.getElementById("round-status");
 const rock_DOM = document.getElementById("rock");
 const paper_DOM = document.getElementById("paper");
@@ -17,42 +16,71 @@ const scissors_DOM = document.getElementById("scissors");
 PlayerScore = 0
 ComputerScore = 0 
 
+function resetGame() {
+    player_score_DOM.innerHTML = "0"
+    pc_score_DOM.innerHTML = "0"
+    round_status_DOM.innerHTML = ""
+    PlayerScore = 0
+    ComputerScore = 0
+}
+
+reset_game_DOM.addEventListener("click", function() {
+    resetGame()
+})
+
+
 
     function game(PlayerSelection) {
         let ComputerSelection = getComputerChoice();
         if (PlayerSelection == "Rock" && ComputerSelection == "Paper") {
-            lose(ComputerScore += 1) 
+            lose(PlayerSelection,ComputerSelection) 
         } else if (PlayerSelection == "Rock" && ComputerSelection == "Scissors") {
-            win(PlayerScore += 1)
+            win(PlayerSelection,ComputerSelection)
         } else if (PlayerSelection == "Paper" && ComputerSelection == "Rock") {
-            win()
+            win(PlayerSelection,ComputerSelection)
         } else if (PlayerSelection == "Paper" && ComputerSelection == "Scissors") {
-            lose()
+            lose(PlayerSelection,ComputerSelection)
         } else if (PlayerSelection == "Scissors" && ComputerSelection == "Rock") {
-            lose()
+            lose(PlayerSelection,ComputerSelection)
         } else if (PlayerSelection == "Scissors" && ComputerSelection == "Paper") {
-            win()
+            win(PlayerSelection,ComputerSelection)
         } else {
             draw()
         }
+
+       
         
     } 
 
 
-
-    function win() {
+    function win(PlayerSelection,ComputerSelection) {
+        PlayerScore++
         player_score_DOM.innerHTML = PlayerScore
-        round_status_DOM.innerHTML = "You won against computer this round"
+        round_status_DOM.innerHTML = PlayerSelection + " beats " + ComputerSelection + " you won this round"
+        getWinner() 
     }
 
-    function lose() {
+    function lose(PlayerSelection,ComputerSelection) {
+        ComputerScore++
         pc_score_DOM.innerHTML = ComputerScore
-        round_status_DOM.innerHTML = "You lost against computer this round"
+        round_status_DOM.innerHTML = ComputerSelection + " beats " + PlayerSelection + " you lost this round"
+        getWinner()
     }
 
     function draw() {
         round_status_DOM.innerHTML = "You draw with the computer this round"
     }
+
+    function getWinner() {
+        if (PlayerScore === 5) {
+            round_status_DOM.textContent = "YEY YOU WON THE WHOLE GAME"
+            PlayerScore = PlayerScore 
+        } else if (ComputerScore === 5) {
+            round_status_DOM.textContent = "OUU YOU LOST THE GAME"
+            
+        }
+    }
+
 
     function choosing() {
         rock_DOM.addEventListener("click",  function() {
@@ -68,8 +96,10 @@ ComputerScore = 0
         })
     }
     
-choosing()
+    choosing()
 
+
+   
 
 
 
